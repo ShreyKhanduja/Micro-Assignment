794f6562c2a13b5a3997f7538db35ebab4666231
Shrey <shereykhanduja@gmail.com>
2022-12-09 21:47:32 +0530

    boilerplate server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
+
+app.listen(port,() => {
+    console.log("Express server listening at http://${host}:${port})");
+})
